c5f09b90ee37a9e388d98d7002c215b6d33867c8
Juno Nguyen <juno.ngx@gmail.com>
2026-02-20 00:13:01 +0800

    Widen month picker in Log view

--- src/app/(app)/log/LogClient.tsx
+++ src/app/(app)/log/LogClient.tsx
@@ -97,7 +97,7 @@ export default function LogClient() {
             value={monthValue}
             onChange={(e) => handleMonthChange(e.target.value)}
             className={`
-                w-40
+                w-42
                 rounded-lg px-3 py-2
                 font-mono text-sm text-foreground
                 bg-default-100 border border-default-200
