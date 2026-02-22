8b7d2614e6ed63090f7043cd04ba87621bc35ac8
Juno Nguyen <juno.ngx@gmail.com>
2026-02-22 12:24:27 +0800

    Set viewport meta config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,6 +36,7 @@ export const viewport: Viewport = {
     width: "device-width",
     initialScale: 1,
     maximumScale: 1,
+    interactiveWidget: "resizes-visual",
 };
 
 export default function RootLayout({
