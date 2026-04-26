b1512e4b248730881b41938c0238fecf11d515de
Juno Nguyen <juno.ngx@gmail.com>
2026-04-27 00:23:27 +0800

    #32 refactor: reorganise constants.ts sections

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,7 +2,7 @@
 export const BASE_URL =
     process.env.NEXT_PUBLIC_BASE_URL ?? "https://pangolog.app";
 
-/* MODAL CLASS NAMES */
+/* CLASSNAMES */
 export const FORM_MODAL_CLASS_NAMES = {
     closeButton: "cursor-pointer",
     body: "overflow-y-auto max-h-[calc(var(--visual-viewport-height,100svh)-10rem)]",
@@ -11,3 +11,31 @@ export const FORM_MODAL_CLASS_NAMES = {
 export const DEFAULT_MODAL_CLASS_NAMES = {
     closeButton: "cursor-pointer",
 };
+
+export const SELECT_CLASSES = `
+    rounded-lg px-3 py-2
+    text-sm text-foreground
+    bg-default-100 border border-default-200
+    cursor-pointer
+`;
+
+/* CONSTANTS */
+export const MONTH_NAMES = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+];
+
+export const YEAR_OPTIONS = Array.from(
+    { length: 21 },
+    (_, i) => new Date().getFullYear() - 10 + i,
+);
