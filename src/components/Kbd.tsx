72c10625f57c2fe0c88be099015c1b29edba605c
Juno Nguyen <juno.ngx@gmail.com>
2026-05-09 11:33:31 +0800

    chore: remove annotation in Kbd

--- src/components/Kbd.tsx
+++ src/components/Kbd.tsx
@@ -5,7 +5,7 @@ interface KbdProps {
 export function Kbd({ children }: KbdProps) {
     return (
         <kbd
-            className={`/* CONTAINER */ /* CONTENT STYLES */ /* VISUAL EFFECTS */ bg-default-100 border-default-200 inline-block rounded border px-1.5 py-0.5 font-mono text-xs`}
+            className="bg-default-100 border-default-200 inline-block rounded border px-1.5 py-0.5 font-mono text-xs"
         >
             {children}
         </kbd>
