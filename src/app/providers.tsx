f1154090cf7889065a2fca778eaf2d2c9c559075
Juno Nguyen <juno.ngx@gmail.com>
2026-06-02 21:30:56 +0800

    #34 feat: bump HeroUI to v3, update theme CSS, remove provider

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { HeroUIProvider } from "@heroui/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ThemeProvider, useTheme } from "next-themes";
 import { useEffect, useState } from "react";
@@ -63,13 +62,11 @@ export function Providers({ children }: { children: React.ReactNode }) {
     return (
         <QueryClientProvider client={queryClient}>
             <ThemeProvider attribute="class" defaultTheme="system">
-                <HeroUIProvider>
-                    {children}
-                    <StoreHydration />
-                    <ThemeColorSync />
-                    <ThemedToaster />
-                    <ServiceWorkerRegistration />
-                </HeroUIProvider>
+                {children}
+                <StoreHydration />
+                <ThemeColorSync />
+                <ThemedToaster />
+                <ServiceWorkerRegistration />
             </ThemeProvider>
         </QueryClientProvider>
     );
