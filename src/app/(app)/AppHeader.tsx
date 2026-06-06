0f30443dab0c5c28a2b3a560a70cc155b38d6dd4
Juno Nguyen <juno.ngx@gmail.com>
2026-06-06 21:07:35 +0800

    #29 fix: theme switch too close to border

--- src/app/(app)/AppHeader.tsx
+++ src/app/(app)/AppHeader.tsx
@@ -6,9 +6,9 @@ import { AppNavbar } from "./AppNavbar";
 
 export function AppHeader() {
     return (
-        <header className="z-40 mt-4 hidden w-full items-center md:flex">
+        <header className="z-40 mt-4 hidden w-full items-end md:flex">
             <AppNavbar />
-            <div className="border-b-foreground flex flex-1 shrink-0 items-center justify-end gap-2 border-b">
+            <div className="border-b-foreground flex flex-1 shrink-0 items-center justify-end gap-2 border-b pb-1">
                 <ThemeSwitcher />
                 <NextLink href="/" className="shrink-0 text-lg font-bold">
                     Pangolog
